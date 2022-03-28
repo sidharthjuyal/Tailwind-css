d7985ccc00782fc8af41fd856f74381214cc8c45
Sidharth juyal <sid.juyal.34@gmail.com>
2022-03-29 01:13:00 +0530

    update

---
+++ config tailwind.js/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    screens: {
+      xsm: '500px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
